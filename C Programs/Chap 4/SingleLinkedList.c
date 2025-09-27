0030c4fcd6dcf24aac8f57ed06f13349ee7b69d6
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-09-27 10:52:14 +0530

    added comments for explain

--- C Programs/Chap 4/SingleLinkedList.c
+++ C Programs/Chap 4/SingleLinkedList.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  //io
+#include <stdlib.h> //malloc
 
 // Node structure
-struct Node
+struct Node // Define the structure for a node in the linked list
 {
     int data;
-    struct Node *next;
+    struct Node *next; // Pointer to the next node
 };
 
-int main()
+int main() // main function
 {
     // Create nodes
-    struct Node *head = NULL;
-    struct Node *second = NULL;
-    struct Node *third = NULL;
+    struct Node *head = NULL;   // Initialize head pointer
+    struct Node *second = NULL; // Initialize second pointer
+    struct Node *third = NULL;  // Initialize third pointer - next node address no idea
 
     // Allocate memory
-    head = (struct Node *)malloc(sizeof(struct Node));
-    second = (struct Node *)malloc(sizeof(struct Node));
-    third = (struct Node *)malloc(sizeof(struct Node));
+    head = (struct Node *)malloc(sizeof(struct Node));   // Allocate memory for head node
+    second = (struct Node *)malloc(sizeof(struct Node)); // Allocate memory for second node
+    third = (struct Node *)malloc(sizeof(struct Node));  // Allocate memory for third node
 
     // Assign data & links
-    head->data = 10;
-    head->next = second;
+    head->data = 10;     // Assign data to head node
+    head->next = second; // Link head node to second node
 
     second->data = 20;
     second->next = third;
